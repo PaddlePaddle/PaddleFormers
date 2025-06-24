48b4e9fc2384d6cc32de7bead5d94088d7063b22
wangyanbo05 <wangyanbo05@baidu.com>
2025-06-24 14:22:12 +0800

    去除jieba相关依赖

--- tests/data/test_tokenizer.py
+++ tests/data/test_tokenizer.py
@@ -14,33 +14,6 @@
 
 import unittest
 
-from paddleformers.data import JiebaTokenizer, Vocab
-from tests.common_test import CpuCommonTest
-from tests.testing_utils import create_test_data
-
-
-class TestJiebaTokenizer(CpuCommonTest):
-    def setUp(self):
-        test_data_file = create_test_data(__file__)
-        self.vocab = Vocab.load_vocabulary(test_data_file, unk_token="[UNK]")
-        self.tokenizer = JiebaTokenizer(self.vocab)
-
-    def test_jieba(self):
-        text = "一万一"
-        token_arr = self.tokenizer.cut(text)
-        idx_arr = self.tokenizer.encode(text)
-        for i, token in enumerate(token_arr):
-            self.check_output_equal(self.vocab(token), idx_arr[i])
-
-        jieba_tokenizer = self.tokenizer.get_tokenizer()
-        jieba_token_arr = jieba_tokenizer.lcut(text, False, True)
-        self.check_output_equal(token_arr, jieba_token_arr)
-
-    def test_unk(self):
-        text = "中国"
-        idx_arr = self.tokenizer.encode(text)
-        self.check_output_equal(self.vocab[self.vocab.unk_token] in idx_arr, True)
-
 
 if __name__ == "__main__":
     unittest.main()
