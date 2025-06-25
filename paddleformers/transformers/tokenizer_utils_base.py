2d8df851be11aabaffe82a0a259edb9c7c295019
miao200years <243062960@qq.com>
2025-06-25 17:05:33 +0800

    fixERR: Add_specialtoken

--- paddleformers/transformers/tokenizer_utils_base.py
+++ paddleformers/transformers/tokenizer_utils_base.py
@@ -1261,7 +1261,7 @@ class SpecialTokensMixin:
         return set_attr
 
     @property
-    @cache
+    #@cache
     def all_special_tokens(self) -> List[str]:
         """
         `List[str]`: All the special tokens (`'<unk>'`, `'<cls>'`, etc.) mapped to class attributes.
@@ -1272,7 +1272,7 @@ class SpecialTokensMixin:
         return all_toks
 
     @property
-    @cache
+    #@cache
     def all_special_tokens_extended(self) -> List[Union[str, AddedToken]]:
         """
         `List[Union[str, AddedToken]]`: All the special tokens (`'<unk>'`, `'<cls>'`, etc.) mapped to class
