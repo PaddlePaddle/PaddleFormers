cdeb6de9da8d370941e7e69b95647826d4f85239
cheng221 <91824655+cheng221@users.noreply.github.com>
2025-09-05 15:30:40 +0800

    fix mistakes  change in tp

--- paddleformers/transformers/conversion_utils.py
+++ paddleformers/transformers/conversion_utils.py
@@ -1278,7 +1278,7 @@ class ConversionMixin:
 
     @classmethod
     def convert_tensor_parallel(
-        cls, model, weight_file: str, config: PretrainedConfig, state_dict=None, ignore_error=False
+        cls, weight_file: str, config: PretrainedConfig, state_dict=None, ignore_error=False
     ) -> None:
         """the entry of converting config and converting model file
 
@@ -1287,7 +1287,7 @@ class ConversionMixin:
             config (PretrainedConfig): the PretrainedConfig instance of model
         """
 
-        name_action_mappings = cls._get_tensor_parallel_mappings(model, config)
+        name_action_mappings = cls._get_tensor_parallel_mappings(config)
         if config.quantization_config.is_weight_quantize():
             name_action_mappings = add_quant_mapping(name_action_mappings, config.quantization_config)
         if state_dict is None:
