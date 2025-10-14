d8ef3334aa0b3f8ea1ccecc3161daf6558a63874
lugimzzz <zhenglujing@baidu.com>
2025-10-14 14:54:32 +0800

    update v0.3.2

--- setup.py
+++ setup.py
@@ -113,7 +113,7 @@ def show():
 
 # only use this file to contral the version
 
-__version__ = "0.3.1.post"
+__version__ = "0.3.2.post"
 
 if os.getenv(PADDLEFORMERS_STABLE_VERSION):
     __version__ = __version__.replace(".post", "")
