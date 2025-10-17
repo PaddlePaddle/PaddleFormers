04121af201adcb874b9ffe591fc4726f5763845d
huangzhiheng02 <565324250@qq.com>
2025-10-17 11:27:49 +0800

    Add model unittest sft tp fp fc

--- scripts/regression/test_dpo.py
+++ scripts/regression/test_dpo.py
@@ -163,7 +163,7 @@ class DPOTrainTest(unittest.TestCase):
         self.dpotrain_tester.assert_result(training_p.returncode, training_p.stdout)
 
         # test training loss
-        EXCEPTED_LOSS = 0.474259
+        EXCEPTED_LOSS = 0.495105
         self.dpotrain_tester.assert_loss(training_p.stdout, EXCEPTED_LOSS)
 
         # test model generate
