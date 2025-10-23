19a314ddaa8570b05c85072ffc9881adbb7306f1
huangzhiheng02 <565324250@qq.com>
2025-10-23 11:28:15 +0800

    fix exit -6

--- scripts/regression/ci_model_unittest.sh
+++ scripts/regression/ci_model_unittest.sh
@@ -111,7 +111,8 @@ if [[ ${FLAGS_enable_CI} == "true" ]] || [[ ${FLAGS_enable_CE} == "true" ]];then
     nvidia-smi
     echo "Check paddle device count"
     python -c "import paddle; print(paddle.device.device_count())"
-    export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+    export NCCL_ASYNC_ERROR_HANDLING=1
+    export FLAGS_enable_exit_on_abnormal=False
     PYTHONPATH=$(pwd) \
     COVERAGE_SOURCE=paddleformers \
     python -m pytest -s -v ${model_unittest_path} > ${log_path}/model_unittest.log 2>&1
