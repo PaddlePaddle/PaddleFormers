adbce59de732164705d3eb94865076f6a8629e70
chen2016013 <111894720+chen2016013@users.noreply.github.com>
2025-10-27 16:45:13 +0800

    Rerun CI

--- examples/experiments/deepseek_v3_pretrain/modeling.py
+++ examples/experiments/deepseek_v3_pretrain/modeling.py
@@ -349,7 +349,6 @@ class MoEGate(PretrainedMoEGate):
                 default_initializer=nn.initializer.Constant(0.0),
             )
             self.e_score_correction_bias.is_distributed = True
-
             self.expert_usage = paddle.zeros(
                 shape=[num_experts],
                 dtype=paddle.int64,
