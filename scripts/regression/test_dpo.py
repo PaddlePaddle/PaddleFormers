1aae9d2afea5fd884fc2d2ff9de3ebbdbca4cf28
huangzhiheng02 <565324250@qq.com>
2025-10-28 11:55:46 +0800

    feat/Add-dop-test-model-resume

--- scripts/regression/test_dpo.py
+++ scripts/regression/test_dpo.py
@@ -126,6 +126,13 @@ class DPOTrainTest(unittest.TestCase):
         # test training loss
         EXCEPTED_LOSS = 0.474242
         self.dpotrain_tester.assert_loss(training_p.stdout, EXCEPTED_LOSS)
+        
+        # test model resume
+        reusme_p = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+        self.dpotrain_tester.assert_result(reusme_p.returncode, reusme_p.stdout)
+
+        EXCEPTED_LOSS = 0.438502
+        self.dpotrain_tester.assert_loss(reusme_p.stdout, EXCEPTED_LOSS)
 
         # test model generate
         EXPECTED_RESULT = paddle.to_tensor([[22407, 90612, 90612, 90612, 90612, 90612, 90612, 90612, 90612, 90612]])
@@ -197,6 +204,13 @@ class DPOTrainTest(unittest.TestCase):
         # test training loss
         EXCEPTED_LOSS = 0.495105
         self.dpotrain_tester.assert_loss(training_p.stdout, EXCEPTED_LOSS)
+        
+        # test model resume
+        reusme_p = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+        self.dpotrain_tester.assert_result(reusme_p.returncode, reusme_p.stdout)
+
+        EXCEPTED_LOSS = 0.495105
+        self.dpotrain_tester.assert_loss(reusme_p.stdout, EXCEPTED_LOSS)
 
         # test model generate
         EXPECTED_RESULT = paddle.to_tensor([[22407, 90612, 90612, 90612, 90612, 90612, 90612, 90612, 90612, 90612]])
