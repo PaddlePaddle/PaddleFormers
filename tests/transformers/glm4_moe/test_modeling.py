aabd06b019a4537dcb5ca9a97598323dfafa1ee5
wangyanbo05 <wangyanbo05@baidu.com>
2025-11-05 20:49:19 +0800

    update tiny model

--- tests/transformers/glm4_moe/test_modeling.py
+++ tests/transformers/glm4_moe/test_modeling.py
@@ -383,7 +383,7 @@ class Glm4MoeModelIntegrationTest(ModelTesterPretrainedMixin, unittest.TestCase)
 
     def test_inference_no_attention(self):
         model = Glm4MoeModel.from_pretrained(
-            "PaddleFormers/tiny-random-glm4moe", download_hub="aistudio", convert_from_hf=False
+            "PaddleFormers/tiny-random-glm4moe", download_hub="aistudio", convert_from_hf=True
         )
         model.eval()
         input_ids = paddle.to_tensor([[0, 345, 232, 328, 740, 140, 1695, 69, 6078, 1588, 2]])
@@ -406,7 +406,7 @@ class Glm4MoeModelIntegrationTest(ModelTesterPretrainedMixin, unittest.TestCase)
 
     def test_inference_with_attention(self):
         model = Glm4MoeModel.from_pretrained(
-            "PaddleFormers/tiny-random-glm4moe", download_hub="aistudio", convert_from_hf=False
+            "PaddleFormers/tiny-random-glm4moe", download_hub="aistudio", convert_from_hf=True
         )
         model.eval()
         input_ids = paddle.to_tensor([[0, 345, 232, 328, 740, 140, 1695, 69, 6078, 1588, 2]])
