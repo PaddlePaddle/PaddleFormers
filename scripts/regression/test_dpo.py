70dad73e5cc05199911eab5635924434fe13605e
Ace-To-HYB <huangyb83@foxmail.com>
2025-11-06 19:17:26 +0800

    fix unittest and ci requirements

--- scripts/regression/test_dpo.py
+++ scripts/regression/test_dpo.py
@@ -143,7 +143,7 @@ class DPOTrainTest(unittest.TestCase):
                 dop_full_reusme_f.write(dop_full_reusme_output)
         self.dpotrain_tester.assert_result(reusme_p.returncode, reusme_p.stdout)
 
-        EXCEPTED_LOSS = 0.691044
+        EXCEPTED_LOSS = 0.691177
         self.dpotrain_tester.assert_loss(reusme_p.stdout, EXCEPTED_LOSS)
 
         # test model generate
@@ -182,7 +182,7 @@ class DPOTrainTest(unittest.TestCase):
         self.dpotrain_tester.assert_result(training_p.returncode, training_p.stdout)
 
         # test training loss
-        EXCEPTED_LOSS = 0.692095
+        EXCEPTED_LOSS = 0.691972
         self.dpotrain_tester.assert_loss(training_p.stdout, EXCEPTED_LOSS)
 
         # test model resume
@@ -244,7 +244,7 @@ class DPOTrainTest(unittest.TestCase):
         self.dpotrain_tester.assert_result(training_p.returncode, training_p.stdout)
 
         # test training loss
-        EXCEPTED_LOSS = 0.692381
+        EXCEPTED_LOSS = 0.692186
         self.dpotrain_tester.assert_loss(training_p.stdout, EXCEPTED_LOSS)
         # test model resume
         reusme_p = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
@@ -295,7 +295,7 @@ class DPOTrainTest(unittest.TestCase):
         self.dpotrain_tester.assert_result(training_p.returncode, training_p.stdout)
 
         # test training loss
-        EXCEPTED_LOSS = 0.692254
+        EXCEPTED_LOSS = 0.692127
         self.dpotrain_tester.assert_loss(training_p.stdout, EXCEPTED_LOSS)
 
         # test model resume
