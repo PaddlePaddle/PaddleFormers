36afaeecab6be0643dfaa56d4e6eed861a02788a
chen2016013 <cx2016013@163.com>
2025-11-06 17:47:25 +0800

    update

--- examples/experiments/deepseek_v3_pretrain/modeling.py
+++ examples/experiments/deepseek_v3_pretrain/modeling.py
@@ -71,6 +71,7 @@ from paddleformers.transformers.conversion_utils import (
     init_name_mappings,
 )
 from paddleformers.transformers.deepseek_v2 import (
+    yarn_get_mscale,
     _expand_2d_mask,
     _make_causal_mask,
 )
@@ -1841,6 +1842,27 @@ class DeepseekV2PretrainingCriterionFast(nn.Layer):
             loss = add_loss(loss, router_loss)
 
         return loss
+
+
+# Inverse axis formula to find dim based on number of rotations
+def yarn_find_correction_dim(num_rotations, dim, base=10000, max_position_embeddings=2048):
+    return (dim * math.log(max_position_embeddings / (num_rotations * 2 * math.pi))) / (2 * math.log(base))
+
+
+# Find axis range bounds based on rotations
+def yarn_find_correction_range(low_rot, high_rot, dim, base=10000, max_position_embeddings=2048):
+    low = math.floor(yarn_find_correction_dim(low_rot, dim, base, max_position_embeddings))
+    high = math.ceil(yarn_find_correction_dim(high_rot, dim, base, max_position_embeddings))
+    return max(low, 0), min(high, dim - 1)  # Clamp values just in case
+
+
+def yarn_linear_ramp_mask(min, max, dim):
+    if min == max:
+        max += 0.001  # Prevent singularity
+
+    linear_func = (paddle.arange(dim, dtype=paddle.float32) - min) / (max - min)
+    ramp_func = paddle.clip(linear_func, 0, 1)
+    return ramp_func
 
 
 class DeepseekV2YarnRotaryEmbedding(DeepseekV2RotaryEmbedding):
