58c927fc1c54e1fb1e2df77e72ba5f93df02ed10
wangyuhao <1799470752@qq.com>
2025-11-07 11:09:04 +0800

    final

--- scripts/regression/test_sft.py
+++ scripts/regression/test_sft.py
@@ -263,7 +263,7 @@ class SFTTrainTest(unittest.TestCase):
         self.sfttrain_tester.assert_loss(reusme_p.stdout, EXCEPTED_LOSS)
         # test model generate
         EXPECTED_RESULT = paddle.to_tensor(
-            [[22407, 120525, 77505, 113631, 47887, 134141, 122487, 61092, 40897, 40601]]
+            [[22407, 120525, 77505, 113631, 47887, 134141, 122487, 61092, 40897, 11806]]
         )
         self.sfttrain_tester.create_and_check_model_generate(output_dir, EXPECTED_RESULT)
 
