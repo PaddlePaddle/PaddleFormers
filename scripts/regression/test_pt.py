4555ce545533bfb73dbc162e6b7ffa7ac2f75261
huangzhiheng02 <565324250@qq.com>
2025-11-07 22:14:49 +0800

    shield generate

--- scripts/regression/test_pt.py
+++ scripts/regression/test_pt.py
@@ -145,7 +145,7 @@ class PTTrainTest(unittest.TestCase):
         self.pttrain_tester.assert_loss(reusme_p.stdout, EXCEPTED_LOSS)
 
         # test model generate
-        EXPECTED_RESULT = paddle.to_tensor([[22407, 90612, 90612, 90612, 90612, 90612, 90612, 90612, 90612, 90612]])
+        # EXPECTED_RESULT = paddle.to_tensor([[22407, 90612, 90612, 90612, 90612, 90612, 90612, 90612, 90612, 90612]])
         # self.pttrain_tester.create_and_check_model_generate(output_dir, EXPECTED_RESULT)
 
     def test_pt_lora(self):
@@ -310,14 +310,14 @@ class PTTrainTest(unittest.TestCase):
         self.pttrain_tester.assert_loss(reusme_p.stdout, EXCEPTED_LOSS)
 
         # test lora merge
-        lora_merge_output_dir = os.path.join(output_dir, "export")
+        # lora_merge_output_dir = os.path.join(output_dir, "export")
         # cli mode
         lora_merge_cmd = ["paddleformers-cli", "export", updated_config_path]
         lora_merge_p = subprocess.run(lora_merge_cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
         self.pttrain_tester.assert_result(lora_merge_p.returncode, lora_merge_p.stdout)
 
         # test lora_merge_model generate
-        EXPECTED_RESULT = paddle.to_tensor(
-            [[22407, 120525, 77505, 113631, 47887, 134141, 122487, 61092, 40897, 40601]]
-        )
+        # EXPECTED_RESULT = paddle.to_tensor(
+        #     [[22407, 120525, 77505, 113631, 47887, 134141, 122487, 61092, 40897, 40601]]
+        # )
         # self.pttrain_tester.create_and_check_model_generate(lora_merge_output_dir, EXPECTED_RESULT)
