49bf26230c5c73c5eef1aeb2e14888a88f308529
zhangbo9674 <zhangbo54@baidu.com>
2025-11-08 13:27:01 +0000

    fix

--- paddleformers/cli/train/tuner.py
+++ paddleformers/cli/train/tuner.py
@@ -42,11 +42,7 @@ def _training_function(config: dict[str, Any]) -> None:
     args = config.get("args")
     model_args, data_args, preprocess_args, generating_args, finetuning_args = get_train_args(args)
 
-    if model_args.stage == "dsv3_pretrain":
-        run_dsv3_pretrain(model_args, data_args, generating_args, finetuning_args)
-        return
-
-    if "VL" in model_args.stage:
+    if "VL" in model_args.stage or model_args.stage == "dsv3_pretrain":
         pass
     elif data_args.dataset_type != "pretrain":
         check_path(data_args.train_dataset_path)
@@ -58,6 +54,8 @@ def _training_function(config: dict[str, Any]) -> None:
     elif model_args.stage == "DPO":
         with paddle.amp.auto_cast(enable=False):
             run_dpo(model_args, data_args, generating_args, finetuning_args)
+    elif model_args.stage == "dsv3_pretrain":
+        run_dsv3_pretrain(model_args, data_args, generating_args, finetuning_args)
     else:
         raise ValueError(f"Unknown task: {model_args.stage}.")
 
