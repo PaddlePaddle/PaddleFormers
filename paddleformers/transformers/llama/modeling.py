1c76af6900bca5497656444a208d72b897ab9d5e
xuexixi <xuexixi@baidu.com>
2025-11-11 13:48:26 +0800

    fix parallel_matmul

--- paddleformers/transformers/llama/modeling.py
+++ paddleformers/transformers/llama/modeling.py
@@ -328,8 +328,6 @@ class LlamaPretrainedModel(PretrainedModel):
 
     @classmethod
     def _get_tensor_parallel_mappings(cls, config: LlamaConfig, is_split=True):
-        if config.run_single_model:
-            return {}
         from ..conversion_utils import split_or_merge_func
 
         fn = split_or_merge_func(
