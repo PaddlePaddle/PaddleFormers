a5f5966215baf980cc27f96f8c8335ea73b430b7
xuexixi <xuexixi@baidu.com>
2025-11-14 17:11:26 +0800

    fix parameters_list default error

--- paddleformers/trainer/trainer.py
+++ paddleformers/trainer/trainer.py
@@ -1493,7 +1493,10 @@ class Trainer:
                 raise ValueError(f"unsupported type: {type(dtensors)}")
         return global_micro_batchs
 
-    def optimizer_step(self, args, model, parameters_list=[]):
+    def optimizer_step(self, args, model, parameters_list=None):
+        if parameters_list is None:
+            parameters_list = []
+
         optimizer_was_run = True
         if not args.enable_auto_parallel and self.args.offload_optim:
             self._reload_optimizer()
@@ -1883,7 +1886,7 @@ class Trainer:
 
                         self.timers and self.timers("forward-backward").stop()
 
-                        parameters_list = []
+                        parameters_list = None
                         if not args.enable_auto_parallel:
                             # Manually collect gradients
                             # Case 1: Use recompute and dp
