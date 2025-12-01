7e574b6718a9846a822c43f2eedcf6d6da4fc5b4
Jonathans575 <1563710292@qq.com>
2025-12-01 03:25:37 +0000

    add pt in new data flwo

--- paddleformers/datasets2/SFTDataset.py
+++ paddleformers/datasets2/SFTDataset.py
@@ -52,6 +52,10 @@ class SFTDataSet(IterableDataset):
         self.use_template = dataset_config["use_template"]
         self.split_multi_turn = dataset_config["split_multi_turn"]
         self.encode_one_turn = dataset_config["encode_one_turn"]
+        self.is_pretraining = dataset_config["is_pretraining"]
+        self.truncate_packing = dataset_config["truncate_packing"]
+        if self.truncate_packing and not self.is_pretraining:
+            logger.warning_once("Truncate packing is only valid in pretraining data flow")
 
         # special token
         self.end_of_response = getattr(self.tokenizer.special_tokens_map, "sep_token", "<|end_of_sentence|>")
@@ -77,125 +81,169 @@ class SFTDataSet(IterableDataset):
 
     def __iter__(self):
         for example in self.mix_datasets:
+            actual_example_num = 1
             system = example.get("system", None)
             tools = example.get("tools", None)
             images = example.get("images", [])
             videos = example.get("videos", [])
             audios = example.get("audios", [])
-            if self.use_template:
-                if self.template_backend == "jinja":
-                    if not self.tokenizer.chat_template:
-                        self.tokenizer.chat_template = NONE_CHAT_TEMPLATE
-                    if self.split_multi_turn:
-                        encoded_pairs = postprocess_fc_sequence(self.tokenizer, example)
-                    else:
-                        encoded_pairs = self.tokenizer.encode_chat_inputs(
-                            example, encode_one_turn=self.encode_one_turn
-                        )
+
+            if is_pretraining:
+                if self.truncate_packing:
+                    # return only tokens
+                    yield self._encode_pretraining_example(example, actual_example_num)
                 else:
-                    # 对多模信息做处理，将messages里面的占位符替换
-                    messages = self.template.mm_plugin.process_messages(
-                        example["messages"], images, videos, audios, self.processor
+                    yield self._postprocess_sequence(example, actual_example_num)
+            else:
+                yield self._postprocess_pretraining_sequence(example, actual_example_num)
+
+    def _postprocess_pretraining_sequence(self, example, actual_example_num):
+        tokens = self._encode_pretraining_example(example, actual_example_num)
+        res_tokens = tokens[:-1]
+        res_labels = tokens[1:]
+        loss_mask = [1] * len(res_tokens)
+        pos_ids = list(range(len(res_tokens)))
+        sequence = Sequence(
+            token_ids=res_tokens,
+            position_ids=pos_ids,
+            labels=res_labels,
+            loss_mask=loss_mask,
+            num_examples=actual_example_num,
+        )
+        return sequence
+
+    def _encode_pretraining_example(self, example, actual_example_num):
+        # tokens
+        content = example["messages"][0]["content"]
+        tokens = self.tokenizer.convert_tokens_to_ids(self.tokenizer.tokenize(content))
+        # Add an EOS token at the end of each sample
+        tokens = tokens + [self.tokenizer.eos_token_id]
+        return tokens
+
+    def _postprocess_sequence(self, example, actual_example_num):
+        """Process code completion examples into token sequences.
+
+        Args:
+            example: The input example containing code components.
+            actual_example_num (int): Number of examples used.
+
+        Returns:
+            Sequence: Processed sequence or None if invalid.
+        """
+        if self.use_template:
+            if self.template_backend == "jinja":
+                if not self.tokenizer.chat_template:
+                    self.tokenizer.chat_template = NONE_CHAT_TEMPLATE
+                if self.split_multi_turn:
+                    encoded_pairs = postprocess_fc_sequence(self.tokenizer, example)
+                else:
+                    encoded_pairs = self.tokenizer.encode_chat_inputs(
+                        example, encode_one_turn=self.encode_one_turn
                     )
-                    # 套template，转ids
-                    encoded_pairs = self.template.encode_multiturn(self.tokenizer, messages, system, tools)
             else:
-                encoded_pairs = self.tokenizer.encode_chat_inputs_with_no_template(
-                    example, encode_one_turn=self.encode_one_turn
+                # 对多模信息做处理，将messages里面的占位符替换
+                messages = self.template.mm_plugin.process_messages(
+                    example["messages"], images, videos, audios, self.processor
                 )
-            # 转input_ids, labels
-            num_reserved_tokens_for_each_dialog = 1
-            num_reserved_tokens_for_each_turn = 8
-
-            cur_len = num_reserved_tokens_for_each_dialog
-
-            turn_index = len(encoded_pairs) - 1
-
-            tokens = []
-            loss_mask = []
-            while turn_index >= 0:
-                tokens_src, tokens_target = encoded_pairs[turn_index]
-                if len(tokens_src) + len(tokens_target) > (
-                    self.max_seq_len + 1 - cur_len - num_reserved_tokens_for_each_turn
-                ):
-                    # If the source (src) exceeds length limit, discard this round of conversation data
-                    # If the target (tgt) exceeds length limit, truncate it
-                    if len(tokens_src) > self.max_seq_len + 1 - cur_len - num_reserved_tokens_for_each_turn:
-                        break
-                    else:
-                        reverse_len = (
-                            self.max_seq_len + 1 - cur_len - num_reserved_tokens_for_each_turn - len(tokens_src)
-                        )
-                        tokens_target = tokens_target[:reverse_len]
-
-                tokens = tokens_src + tokens_target + tokens
-
-                loss_mask = (
-                    [0] * (len(tokens_src) - 1) + [example["label"][turn_index]] * (len(tokens_target) + 1) + loss_mask
-                )
-                assert len(tokens) == len(loss_mask), f"{len(tokens)}-{len(loss_mask)}"
-
-                cur_len = len(tokens)
-
-                turn_index -= 1
-
-            # Not even one turn can be added, so need to do warning and skip this example
-            if len(tokens) <= num_reserved_tokens_for_each_dialog + num_reserved_tokens_for_each_turn:
-                try:
-                    # For print log
-                    sub_src = example["messages"][0]["content"].strip()[:50]
-                    sub_tgt = example["messages"][-1]["content"].strip()[-50:]
-                    if len(tokens) > 0:
-                        logger.warning(f"This data is too short: '{{'src':[{sub_src}, ……],'tgt':[……{sub_tgt}]}}'")
-                    else:
-                        logger.warning(f"This data is too long: '{{'src':[{sub_src}, ……],'tgt':[……{sub_tgt}]}}'")
-                except Exception:
-                    logger.warning("[SKIP] wrong example")
-
-            if self.begin_token_id is not None and self.end_of_response_id is not None:
-                if tokens[0] != self.begin_token_id:
-                    tokens = [self.begin_token_id] + tokens
-                    loss_mask = [0] + loss_mask
-
-                if len(tokens) > self.max_seq_len:
-                    raise RuntimeError(f"token_ids is too long: {len(tokens)}")
-
-                # Add EOS token at the end
-                del tokens[-1]
-                del loss_mask[-1]
-                labels = tokens[1:] + [self.tokenizer.eos_token_id]
-
-                # end_of_response is a special token that indicates the end of the turn.
-                # end_token is a special token that indicates the end of the answer.
-                labels = [
-                    label if label != self.end_of_response_id else self.tokenizer.eos_token_id for label in labels
-                ]
-            else:
-                # labels = tokens[1:] + [self.tokenizer.eos_token_id]
-                # tokens = tokens[:-1] + [self.tokenizer.eos_token_id]
-                labels = tokens[1:] + [-100]
-                if len(tokens) > self.max_seq_len:
-                    raise RuntimeError(f"token_ids is too long: {len(tokens)}")
-
-            pos_ids = list(range(len(tokens)))
-
-            if sum(loss_mask) == 0:
-                logger.warning(f"[SKIP] all labels set to 0: {example}")
-                return None
-
-            assert len(tokens) == len(loss_mask), f"{len(tokens)}-{len(loss_mask)}"
-            assert len(tokens) == len(labels), f"{len(tokens)}-{len(labels)}"
-
-            yield Sequence(
-                token_ids=tokens,
-                position_ids=pos_ids,
-                labels=labels,
-                loss_mask=loss_mask,
-                num_examples=1,
-                images=images,
-                videos=videos,
-                audios=audios,
+                # 套template，转ids
+                encoded_pairs = self.template.encode_multiturn(self.tokenizer, messages, system, tools)
+        else:
+            encoded_pairs = self.tokenizer.encode_chat_inputs_with_no_template(
+                example, encode_one_turn=self.encode_one_turn
             )
+        # 转input_ids, labels
+        num_reserved_tokens_for_each_dialog = 1
+        num_reserved_tokens_for_each_turn = 8
+
+        cur_len = num_reserved_tokens_for_each_dialog
+
+        turn_index = len(encoded_pairs) - 1
+
+        tokens = []
+        loss_mask = []
+        while turn_index >= 0:
+            tokens_src, tokens_target = encoded_pairs[turn_index]
+            if len(tokens_src) + len(tokens_target) > (
+                self.max_seq_len + 1 - cur_len - num_reserved_tokens_for_each_turn
+            ):
+                # If the source (src) exceeds length limit, discard this round of conversation data
+                # If the target (tgt) exceeds length limit, truncate it
+                if len(tokens_src) > self.max_seq_len + 1 - cur_len - num_reserved_tokens_for_each_turn:
+                    break
+                else:
+                    reverse_len = (
+                        self.max_seq_len + 1 - cur_len - num_reserved_tokens_for_each_turn - len(tokens_src)
+                    )
+                    tokens_target = tokens_target[:reverse_len]
+
+            tokens = tokens_src + tokens_target + tokens
+
+            loss_mask = (
+                [0] * (len(tokens_src) - 1) + [example["label"][turn_index]] * (len(tokens_target) + 1) + loss_mask
+            )
+            assert len(tokens) == len(loss_mask), f"{len(tokens)}-{len(loss_mask)}"
+
+            cur_len = len(tokens)
+
+            turn_index -= 1
+
+        # Not even one turn can be added, so need to do warning and skip this example
+        if len(tokens) <= num_reserved_tokens_for_each_dialog + num_reserved_tokens_for_each_turn:
+            try:
+                # For print log
+                sub_src = example["messages"][0]["content"].strip()[:50]
+                sub_tgt = example["messages"][-1]["content"].strip()[-50:]
+                if len(tokens) > 0:
+                    logger.warning(f"This data is too short: '{{'src':[{sub_src}, ……],'tgt':[……{sub_tgt}]}}'")
+                else:
+                    logger.warning(f"This data is too long: '{{'src':[{sub_src}, ……],'tgt':[……{sub_tgt}]}}'")
+            except Exception:
+                logger.warning("[SKIP] wrong example")
+
+        if self.begin_token_id is not None and self.end_of_response_id is not None:
+            if tokens[0] != self.begin_token_id:
+                tokens = [self.begin_token_id] + tokens
+                loss_mask = [0] + loss_mask
+
+            if len(tokens) > self.max_seq_len:
+                raise RuntimeError(f"token_ids is too long: {len(tokens)}")
+
+            # Add EOS token at the end
+            del tokens[-1]
+            del loss_mask[-1]
+            labels = tokens[1:] + [self.tokenizer.eos_token_id]
+
+            # end_of_response is a special token that indicates the end of the turn.
+            # end_token is a special token that indicates the end of the answer.
+            labels = [
+                label if label != self.end_of_response_id else self.tokenizer.eos_token_id for label in labels
+            ]
+        else:
+            # labels = tokens[1:] + [self.tokenizer.eos_token_id]
+            # tokens = tokens[:-1] + [self.tokenizer.eos_token_id]
+            labels = tokens[1:] + [-100]
+            if len(tokens) > self.max_seq_len:
+                raise RuntimeError(f"token_ids is too long: {len(tokens)}")
+
+        pos_ids = list(range(len(tokens)))
+
+        if sum(loss_mask) == 0:
+            logger.warning(f"[SKIP] all labels set to 0: {example}")
+            return None
+
+        assert len(tokens) == len(loss_mask), f"{len(tokens)}-{len(loss_mask)}"
+        assert len(tokens) == len(labels), f"{len(tokens)}-{len(labels)}"
+
+        return Sequence(
+            token_ids=tokens,
+            position_ids=pos_ids,
+            labels=labels,
+            loss_mask=loss_mask,
+            num_examples=1,
+            images=images,
+            videos=videos,
+            audios=audios,
+        )
 
 
 class SFTPackingDataset(IterableDataset):
@@ -205,6 +253,7 @@ class SFTPackingDataset(IterableDataset):
         self.greedy_intokens = dataset_config["greedy_intokens"]
         self.max_seq_len = dataset_config["max_seq_len"]
         self.is_valid = dataset_config["is_valid"]
+        self.truncate_packing = dataset_config["truncate_packing"]
 
         self.estimate = False
         # The number of valid samples and skipped samples in estimation
@@ -220,6 +269,76 @@ class SFTPackingDataset(IterableDataset):
     def __iter__(self):
 
         dataset_iterator = iter(self.processed_dataset)
+
+        if self.truncate_packing:
+            all_tokenized_tokens = []
+            for _ in range(len(self.mix_datasets)):
+                example = next(dataset_iterator)
+                actual_example_num = 1
+                tokens = example
+                if tokens is None:
+                    if self.estimate:
+                        self.unused_samples += actual_example_num
+                    continue
+                if self.estimate:
+                    self.used_samples += actual_example_num
+
+                all_tokenized_tokens.extend(tokens)
+
+                while len(all_tokenized_tokens) >= self.max_seq_len:
+                    cut_tokens = all_tokenized_tokens[: self.max_seq_len]
+                    # Add an EOS token at the position of data truncation
+                    if cut_tokens[-1] != self.tokenizer.eos_token_id:
+                        cut_tokens = cut_tokens + [self.tokenizer.eos_token_id]
+                    all_tokenized_tokens = all_tokenized_tokens[self.max_seq_len :]
+
+                    res_tokens = cut_tokens[:-1]
+                    res_labels = cut_tokens[1:]
+                    loss_mask = [1] * len(res_tokens)
+                    pos_ids = list(range(len(res_tokens)))
+                    sequence = Sequence(
+                        token_ids=res_tokens,
+                        position_ids=pos_ids,
+                        labels=res_labels,
+                        loss_mask=loss_mask,
+                        num_examples=actual_example_num,
+                    )
+                    batch_sequence = [sequence]
+                    yield batch_sequence
+
+                    if self.estimate:
+                        self.used_estimate_samples += actual_example_num
+                        self.print_max_steps_estimate_progress()
+                        if self.used_estimate_samples >= self.max_estimate_samples:
+                            self.used_estimate_samples = 0
+                            # Set flag to False and yield empty list to signal the end of estimation
+                            self.estimate = False
+                            yield []
+
+            # If the entire dataset has been fully traversed, return the remaining data.
+            if len(all_tokenized_tokens) > 0:
+                cut_tokens = all_tokenized_tokens
+                cut_tokens = cut_tokens + [self.tokenizer.eos_token_id]
+                res_tokens = cut_tokens[:-1]
+                res_labels = cut_tokens[1:]
+                loss_mask = [1] * len(res_tokens)
+                pos_ids = list(range(len(res_tokens)))
+                sequence = Sequence(
+                    token_ids=res_tokens,
+                    position_ids=pos_ids,
+                    labels=res_labels,
+                    loss_mask=loss_mask,
+                    num_examples=actual_example_num,
+                )
+                batch_sequence = [sequence]
+                yield batch_sequence
+                if self.estimate:
+                    self.used_estimate_samples += actual_example_num
+                    if self.used_estimate_samples >= self.max_estimate_samples:
+                        self.used_estimate_samples = 0
+                        # Set flag to False and yield empty list to signal the end of estimation
+                        self.estimate = False
+                        yield []
 
         if not self.packing:
             for _ in range(len(self.processed_dataset)):
@@ -358,40 +477,3 @@ class SFTPackingDataset(IterableDataset):
 
         return generate_packs
 
-
-if __name__ == "__main__":
-
-    model_path = "/root/paddlejob/workspace/env/output/lrl/PaddleFormers/models/Qwen2.5-VL-3B-Instruct"
-    tokenizer = AutoTokenizer.from_pretrained(model_path)
-    processor = AutoProcessor.from_pretrained(model_path)
-    dataset_config = {
-        "tokenizer": tokenizer,
-        "processor": processor,
-        "random_seed": 42,
-        "random_shuffle": True,
-        "num_replicas": 1,
-        "rank": 0,
-        "mix_strategy": "concat",
-        "num_samples_each_epoch": 6000000,
-        "packing": True,
-        "greedy_intokens": True,
-        "max_seq_len": 8192,
-        "encode_one_turn": True,
-        "use_template": True,
-        "reverse": True,
-        "is_valid": False,
-    }
-    dataset_config.update(
-        {
-            "task_group": "/root/paddlejob/workspace/env/output/lrl/PaddleFormers/data/vl/experiment.jsonl",
-            "task_group_prob": "1.0",
-            "sub_dataset_type": "erniekit",
-        }
-    )
-
-    train_dataset = SFTDataSet(**dataset_config)
-    train_packing_dataset = SFTPackingDataset(train_dataset, **dataset_config)
-
-    for item in train_packing_dataset:
-        print(item)
-        break
