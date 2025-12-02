444289bf21d9e47151b6f27af7deaaecd92a1f10
gushiwei <gushiwei@baidu.com>
2025-12-02 10:30:26 +0000

    add paddlefleet

--- scripts/regression/ci_model_unittest.sh
+++ scripts/regression/ci_model_unittest.sh
@@ -31,7 +31,7 @@ install_requirements() {
     python -m pip install -r requirements.txt --extra-index-url https://www.paddlepaddle.org.cn/packages/nightly/cu126/
     python -m pip install -r requirements-dev.txt --extra-index-url https://www.paddlepaddle.org.cn/packages/nightly/cu126/
     python -m pip install -r tests/requirements.txt --extra-index-url https://www.paddlepaddle.org.cn/packages/nightly/cu126/
-    python -m pip install --no-cache-dir ${paddle} --no-dependencies --progress-bar off --force-reinstall
+    # python -m pip install --no-cache-dir ${paddle} --no-dependencies --progress-bar off --force-reinstall
     python -c "import paddle;print('paddle');print(paddle.__version__);print(paddle.version.show())" >> ${log_path}/commit_info.txt
     python setup.py bdist_wheel > /dev/null
     python -m pip install  dist/p****.whl
