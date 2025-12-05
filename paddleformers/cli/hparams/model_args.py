e7bd4ce86da69449cec9b2c74b5f6889618cb54d
ChenZhiyang <1792266893@qq.com>
2025-12-05 11:09:41 +0800

    support fleet save_pretrained from_pretrained

--- paddleformers/cli/hparams/model_args.py
+++ paddleformers/cli/hparams/model_args.py
@@ -86,11 +86,11 @@ class ModelArguments:
         metadata={"help": "GPT3 model, use fast layernorm"},
     )
     fuse_attention_qkv: bool = field(
-        default=None,
+        default=True,
         metadata={"help": "whether to fuse attention qkv"},
     )
     fuse_attention_ffn: bool = field(
-        default=None,
+        default=True,
         metadata={"help": "whether to fuse first up and gate proj in mlp block"},
     )
     attn_impl: str = field(default="flashmask", metadata={"help": "Attention implementation"})
