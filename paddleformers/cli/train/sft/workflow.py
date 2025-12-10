504ceba693e983890072e3bfff1e60b6cd21b867
zhengshengning <ningzhengsheng@baidu.com>
2025-12-10 10:10:42 +0800

    solve fav2

--- paddleformers/cli/train/sft/workflow.py
+++ paddleformers/cli/train/sft/workflow.py
@@ -21,14 +21,6 @@ from functools import partial
 
 import paddle
 
-is_sm90 = (
-    paddle.base.core.is_compiled_with_cuda()
-    and paddle.device.cuda.get_device_capability()[0] == 9
-    and paddle.device.cuda.get_device_capability()[1] == 0
-)
-if is_sm90:
-    os.environ["FLAGS_flash_attn_version"] = "3"
-
 from paddleformers.data.causal_dataset import (
     build_train_valid_test_datasets,
     check_data_split,
@@ -201,15 +193,6 @@ def run_sft(
     # (Liuting) Not support acc calculation now due to MTP.
     if "DeepseekV3" in str(model_config.architectures):
         training_args.prediction_loss_only = True
-    # sink_attention v2 not support packing=false Now
-
-    if (
-        "GptOss" in str(model_config.architectures)
-        and data_args.packing is False
-        and model_args.attn_impl == "flashmask"
-    ):
-        if not is_sm90:
-            model_args.attn_impl = "eager"
 
     LlmMetaConfig.set_llm_config(model_config, training_args)
     model_config.use_fast_layer_norm = model_args.use_fast_layer_norm
