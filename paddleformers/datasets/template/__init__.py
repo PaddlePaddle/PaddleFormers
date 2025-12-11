b538d203ab10d385d3931d05e60a42f414a48a4e
forBlank <1422848728@qq.com>
2025-12-11 11:38:07 +0800

    Lazy import PaddleOCRVLPlugin

--- paddleformers/datasets/template/__init__.py
+++ paddleformers/datasets/template/__init__.py
@@ -31,6 +31,7 @@ import_structure = {
         "_check_video_is_nested_images",
         "MMPluginMixin",
         "BasePlugin",
+        "PaddleOCRVLPlugin",
         "Qwen2VLPlugin",
         "Qwen3VLPlugin",
         "register_mm_plugin",
