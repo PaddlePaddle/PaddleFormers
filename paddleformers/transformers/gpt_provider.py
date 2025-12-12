8a5f7fec107a7e47ef222e62806602b2d6c1eaa7
xiaoguoguo626807 <wangruting@baidu.com>
2025-12-12 08:26:05 +0000

    fix vp judge

--- paddleformers/transformers/gpt_provider.py
+++ paddleformers/transformers/gpt_provider.py
@@ -150,7 +150,7 @@ class GPTModelProvider(GPTConfig, ModelProviderMixin[GPTModel]):
             GPTModel: Configured PaddleFleet GPT model instance
         """
         assert HAS_PADDLEFLEET
-        vp_size = self.virtual_pipeline_model_parallel_size > 1
+        vp_size = self.virtual_pipeline_model_parallel_size > 1 and self.virtual_pipeline_model_parallel_size
         pp_size = self.pipeline_model_parallel_size
 
         is_pipeline_asymmetric = getattr(self, "account_for_embedding_in_pipeline_split", False) or getattr(
