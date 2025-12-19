9196a09cf01aef9bbcb26e43dd4272f7e8619db5
Wennie396 <wn295448@buaa.edu.cn>
2025-12-19 16:57:54 +0800

    cp split inputs for fleet model in trainer

--- paddleformers/transformers/glm4_moe/modeling.py
+++ paddleformers/transformers/glm4_moe/modeling.py
@@ -1530,7 +1530,6 @@ class Glm4MoeForCausalLMFleet(Glm4MoePreTrainedModel):
         model_provider_class = GLMMoEModelProvider
         model_provider = model_provider_class.from_config(config)
         gpt_model = model_provider.provide()
-        gpt_model.is_fleet = True
         gpt_model._gen_aoa_config = cls._gen_aoa_config
         gpt_model._gen_inv_aoa_config = cls._gen_inv_aoa_config
         gpt_model._get_tensor_parallel_mappings = cls._get_tensor_parallel_mappings
@@ -1690,7 +1689,6 @@ class Glm4MoeForCausalLMPipeFleet(Glm4MoePreTrainedModel, GeneralModelForCausalL
         model_provider_class = GLMMoEModelProvider
         model_provider = model_provider_class.from_config(config)
         gpt_model = model_provider.provide()
-        gpt_model.is_fleet = True
         gpt_model._gen_aoa_config = cls._gen_aoa_config
         gpt_model._gen_inv_aoa_config = cls._gen_inv_aoa_config
         gpt_model._get_tensor_parallel_mappings = cls._get_tensor_parallel_mappings
