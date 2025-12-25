d400106b9292e8934a4d432899a8774243af6428
Ace-To-HYB <huangyb83@foxmail.com>
2025-12-25 21:56:59 +0800

    fix codestyle

--- paddleformers/transformers/tokenizer_utils.py
+++ paddleformers/transformers/tokenizer_utils.py
@@ -22,7 +22,6 @@ from functools import wraps
 from typing import Any, Dict, List, Optional, Tuple, Union
 
 from tokenizers import AddedToken  # noqa: F401
-
 from transformers import BatchEncoding
 from transformers.tokenization_utils import (
     PreTrainedTokenizer as PreTrainedTokenizer_tf,
