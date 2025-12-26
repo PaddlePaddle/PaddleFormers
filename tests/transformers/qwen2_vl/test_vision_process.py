7fef5caeef393695d3fb86b9d075c87932d4fa27
Ace-To-HYB <huangyb83@foxmail.com>
2025-12-26 16:34:01 +0800

    Fix backend conflict between PreTrainedTokenizerFast and video loader

--- tests/transformers/qwen2_vl/test_vision_process.py
+++ tests/transformers/qwen2_vl/test_vision_process.py
@@ -247,7 +247,7 @@ class TestQwenVisionProcessing(unittest.TestCase):
         if not paddle.to_tensor([0]).place.is_gpu_place():
             self.skipTest("No GPU currently available/allocated")
         ele = {"video": self.test_video_url}
-        result = vision_process.fetch_video(ele, backend="decord")
+        result = vision_process.fetch_video(ele, video_backend="decord")
 
         self.assertIsInstance(result, paddle.Tensor)
 
@@ -257,7 +257,7 @@ class TestQwenVisionProcessing(unittest.TestCase):
         if not paddle.to_tensor([0]).place.is_gpu_place():
             self.skipTest("No GPU currently available/allocated")
         ele = {"video": self.test_video_url}
-        result = vision_process.fetch_video(ele, backend="paddlecodec")
+        result = vision_process.fetch_video(ele, video_backend="paddlecodec")
 
         import torchcodec
 
