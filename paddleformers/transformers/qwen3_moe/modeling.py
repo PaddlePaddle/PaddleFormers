afda2ced98c15bb4b7de1520859f97c49d9f6daf
tianlef <1095012807@qq.com>
2025-12-26 20:00:56 +0800

    add qwen3 code change

--- paddleformers/transformers/qwen3_moe/modeling.py
+++ paddleformers/transformers/qwen3_moe/modeling.py
@@ -1244,7 +1244,7 @@ class Qwen3MoeForCausalLMFleet(Qwen3MoePretrainedModel):
         config.tensor_model_parallel_size = max(config.tensor_model_parallel_size, 1)
         config.context_parallel_size = max(config.context_parallel_size, 1)
         config.pipeline_model_parallel_size = max(config.pipeline_model_parallel_size, 1)
-        config.virtual_pipeline_model_parallel_size = max(config.virtual_pipeline_mode, 1)
+        config.virtual_pipeline_model_parallel_size = max(config.virtual_pipeline_model_parallel_size, 1)
         config.expert_model_parallel_size = max(config.expert_model_parallel_size, 1)
 
         model_provider_class = Qwen3MoEModelProvider
