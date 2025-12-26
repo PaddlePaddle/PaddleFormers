ca67a4c02c99b30fdd8cb164f7492ce169544e32
llbdyiu66 <llbdyiu66@users.noreply.github.com>
2025-12-26 17:46:16 +0800

    fix

--- examples/tools/trans_paddlenlp2hf.py
+++ examples/tools/trans_paddlenlp2hf.py
@@ -33,8 +33,10 @@ def parse_arguments():
         argparse.Namespace: An object containing all parsed command line arguments.
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument("--paddle_model_dir", required=True, type=str, help="Input PaddlePaddle model directory path.")
-    parser.add_argument("--torch_model_dir", required=True, type=str, help="Output PyTorch model directory path.")
+    parser.add_argument(
+        "--paddlenlp_model_path", required=True, type=str, help="Input PaddleNLP model directory path."
+    )
+    parser.add_argument("--hf_model_path", required=True, type=str, help="Output HF model directory path.")
     parser.add_argument("--max_shard_size", default="4GB", type=str, help="The maximum size of each sub-checkpoint.")
     return parser.parse_args()
 
@@ -56,24 +58,24 @@ def load_safetensors_state_dict(input_dir):
     return state_dict
 
 
-def trans_paddle2torch():
+def trans_paddlenlp2hf():
     args = parse_arguments()
 
-    state_dict = load_safetensors_state_dict(args.paddle_model_dir)
+    state_dict = load_safetensors_state_dict(args.paddlenlp_model_path)
     model = AutoModelForCausalLM.from_pretrained(
-        args.paddle_model_dir,
+        args.paddlenlp_model_path,
         state_dict=state_dict,
         convert_from_hf=False,
     )
     model.save_pretrained(
-        args.torch_model_dir,
+        args.hf_model_path,
         max_shard_size=args.max_shard_size,
         save_checkpoint_format="flex_checkpoint",
         save_to_hf=True,
     )
 
     # copy rest files
-    for filename in os.listdir(args.paddle_model_dir):
+    for filename in os.listdir(args.paddlenlp_model_path):
         if (
             filename.endswith(".safetensors")
             or filename.startswith("model")
@@ -81,10 +83,10 @@ def trans_paddle2torch():
             or filename.endswith(".pdparams")
         ):
             continue
-        src_file = os.path.join(args.paddle_model_dir, filename)
-        dst_file = os.path.join(args.torch_model_dir, filename)
+        src_file = os.path.join(args.paddlenlp_model_path, filename)
+        dst_file = os.path.join(args.hf_model_path, filename)
         shutil.copy2(src_file, dst_file)
 
 
 if __name__ == "__main__":
-    trans_paddle2torch()
+    trans_paddlenlp2hf()
