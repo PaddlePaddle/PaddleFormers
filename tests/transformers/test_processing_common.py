a3fe03221e4a71713a3671ebb7ab38809263861b
Ace-To-HYB <huangyb83@foxmail.com>
2026-01-06 16:52:14 +0800

    Support for legacy_serialization to save components separately

--- tests/transformers/test_processing_common.py
+++ tests/transformers/test_processing_common.py
@@ -173,7 +173,7 @@ class ProcessorTesterMixin:
         processor_first = self.get_processor()
 
         with tempfile.TemporaryDirectory() as tmpdir:
-            saved_files = processor_first.save_pretrained(tmpdir)
+            saved_files = processor_first.save_pretrained(tmpdir, legacy_serialization=False)
             check_json_file_has_correct_format(saved_files[0])
 
             # Load it back and check if loaded correctly
