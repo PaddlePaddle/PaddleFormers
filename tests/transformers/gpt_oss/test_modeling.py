75f36aab480cc30651e51d88d861d5d565b6df68
zjjlivein <1834792141@qq.com>
2026-01-14 23:02:49 +0800

    update

--- tests/transformers/gpt_oss/test_modeling.py
+++ tests/transformers/gpt_oss/test_modeling.py
@@ -466,7 +466,6 @@ class GptOssCompatibilityTest(unittest.TestCase):
             )
         )
 
-    @unittest.skip("TODO: Temporarily skipped")
     @require_package("transformers", "torch")
     def test_GptOss_converter_from_local_dir(self):
         with tempfile.TemporaryDirectory() as tempdir:
@@ -498,7 +497,6 @@ class GptOssCompatibilityTest(unittest.TestCase):
                 )
             )
 
-    @unittest.skip("TODO: Temporarily skipped")
     @parameterized.expand([("GptOssModel",), ("GptOssForCausalLM",)])
     @require_package("transformers", "torch")
     def test_GptOss_classes_from_local_dir(self, class_name, pytorch_class_name: str | None = None):
