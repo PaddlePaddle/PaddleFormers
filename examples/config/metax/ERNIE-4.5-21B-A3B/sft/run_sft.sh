f82f3992385bfd86405f6b39d6914883a60a57ad
xuanyuanminzheng <xuanyuanminzheng@126.com>
2026-01-16 16:28:25 +0800

    fix template.

--- examples/config/metax/ERNIE-4.5-21B-A3B/sft/run_sft.sh
+++ examples/config/metax/ERNIE-4.5-21B-A3B/sft/run_sft.sh
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 paddleformers-cli train examples/config/metax/ERNIE-4.5-21B-A3B/sft.yaml
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 paddleformers-cli train examples/config/metax/ERNIE-4.5-21B-A3B/sft/sft.yaml
