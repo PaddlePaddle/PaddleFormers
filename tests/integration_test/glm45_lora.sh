efc979e655499a71763c1b72e82c9a0a19f96c1f
xiaoguoguo626807 <wangruting@baidu.com>
2026-01-28 03:03:44 +0000

    modify step

--- tests/integration_test/glm45_lora.sh
+++ tests/integration_test/glm45_lora.sh
@@ -84,7 +84,7 @@ fi
 
 log_loss_file=${log_file%.*}_loss.${log_file##*.}
 python $root_dir/PaddleFormers/tests/integration_test/check_loss.py \
-   --compare_step 100 \
+   --compare_step 10 \
    --log_file ./${log_file} \
    --log_loss_file ./${log_loss_file} \
    --gt_file ./${gt_loss_file}
