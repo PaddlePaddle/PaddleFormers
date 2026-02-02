70b0df323a9a335e4239a97d103244f22651eee4
Ace-To-HYB <huangyb83@foxmail.com>
2026-02-02 19:03:32 +0800

    [Cherry-Pick]Fix(CI): Fix ffmpeg install#3780

--- tests/transformers/qwen2_vl/test_vision_process.py
+++ tests/transformers/qwen2_vl/test_vision_process.py
@@ -260,6 +260,8 @@ class TestQwenVisionProcessing(unittest.TestCase):
         if not getattr(torchcodec, "__is_paddle_compatible_library__", None):
             raise RuntimeError("Could not import 'torchcodec'. Please ensure it is installed.")
 
+        print("testing ffmpeg install")
+
         self.assertIsInstance(result, paddle.Tensor)
 
     def test_fetch_video_with_frame_list(self):
