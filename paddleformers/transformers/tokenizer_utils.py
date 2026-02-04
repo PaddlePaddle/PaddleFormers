8a2f8f381a8d01bf0664929557aa4539afa498e5
Ace-To-HYB <huangyb83@foxmail.com>
2026-02-04 17:04:44 +0800

    Fix tokenizer saving

--- paddleformers/transformers/tokenizer_utils.py
+++ paddleformers/transformers/tokenizer_utils.py
@@ -383,7 +383,7 @@ class PaddleTokenizerMixin:
 
         # NOTE: Compatibility fix for ERNIE tokenizer vocabulary saving
         if self.__class__.__name__ == "LlamaTokenizer":
-            out_vocab_file = self.save_vocabulary(save_directory, **kwargs)
+            out_vocab_file = self.save_vocabulary(save_directory, kwargs.get("filename_prefix"))
             save_files = save_files + out_vocab_file
 
         return save_files
