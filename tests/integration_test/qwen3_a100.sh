9e06c6a5f50344453e76b713c4964bbc0191dde0
tianlef <1095012807@qq.com>
2026-02-11 00:29:44 +0800

    fix

--- tests/integration_test/qwen3_a100.sh
+++ tests/integration_test/qwen3_a100.sh
@@ -74,8 +74,8 @@ export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 
 unset http_proxy https_proxy
 
-log_file=qwen_$step_a100.txt
-gt_loss_file=qwen_${step}_a100_multi_card_gt_loss.txt
+log_file=qwen_${step}_multi_card_a100.txt
+gt_loss_file=qwen_${step}_multi_card_a100_gt_loss.txt
 
 set +e
 NNODES=1 MASTER_ADDR=$master MASTER_PORT=$port coverage run $(which paddleformers-cli) train $config_yaml 2>&1 | tee ./${log_file}
@@ -94,6 +94,7 @@ if [ $exit_code -ne 0 ]; then
 else
     echo "Test passed."
 fi
+
 
 
 export repo_name=PaddleFleet
