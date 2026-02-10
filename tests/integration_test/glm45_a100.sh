1f8bba70f56b20cda0461b958c5408ddf1d21e9a
tianlef <1095012807@qq.com>
2026-02-11 00:08:11 +0800

    fix glm sft memory

--- tests/integration_test/glm45_a100.sh
+++ tests/integration_test/glm45_a100.sh
@@ -37,7 +37,6 @@ if [[ ${step} == "pt" ]]; then
     | .per_device_train_batch_size = 1
     | .use_expert_parallel = false
     | .stage1_overlap = false
-    | .tensor_model_parallel_size = 2
     | .train_dataset_path = strenv(data_dir) + "/train.jsonl"
     | .eval_dataset_path = strenv(data_dir) + "/eval.jsonl"
     | .model_name_or_path = strenv(cur_dir) + "/GLM-4.5-Air"
@@ -56,8 +55,6 @@ elif [[ ${step} == "sft" ]]; then
     | .use_expert_parallel = false
     | .expert_model_parallel_size = 1
     | .per_device_train_batch_size = 1
-    | .tensor_model_parallel_size = 2
-    | .pipeline_model_parallel_size = 4
     | .stage1_overlap = false
     | .num_empty_layers_add_in_head = 0
     | .output_dir = strenv(cur_dir) + "/checkpoints/glm_full_pp_ckpts"' \
@@ -74,9 +71,8 @@ elif [[ ${step} == "lora" ]]; then
       | .output_dir = strenv(cur_dir) + "/checkpoints/glm_single_lora_ckps"
       | .num_empty_layers_add_in_tail = 0
       | .use_expert_parallel = false
-      | .tensor_model_parallel_size = 2
-      | .pipeline_model_parallel_size = 4
       | .expert_model_parallel_size = 1
+      | .tensorwise_offload_optimizer = true
       | del(.moe_token_dispatcher_type)' \
     $config_yaml > ${config_yaml}.tmp
   mv ${config_yaml}.tmp $config_yaml
@@ -89,7 +85,6 @@ elif [[ ${step} == "dpo" ]]; then
       | .model_name_or_path = strenv(cur_dir) + "/checkpoints/glm_full_pp_ckpts"
       | .logging_dir = strenv(cur_dir) + "/glm_full_dpo_vdl_log"
       | .num_empty_layers_add_in_tail = 0
-      | .tensor_model_parallel_size = 2
       | .pipeline_model_parallel_size = 4
       | .use_expert_parallel = false
       | .expert_model_parallel_size = 1
@@ -107,8 +102,6 @@ elif [[ ${step} == "dpo_lora" ]]; then
       | .logging_dir = strenv(cur_dir) + "/glm_full_dpo_lora_vdl_log"
       | .use_expert_parallel = false
       | .expert_model_parallel_size = 1
-      | .tensor_model_parallel_size = 2
-      | .pipeline_model_parallel_size = 4
       | .output_dir = strenv(cur_dir) + "/checkpoints/glm_full_dpo_lora_ckpts"' \
     $config_yaml > ${config_yaml}.tmp
   mv ${config_yaml}.tmp $config_yaml
