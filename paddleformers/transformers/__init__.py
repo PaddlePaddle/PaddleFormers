dfac0cbaeced7f5d9f5aa1621faa31f40c34b1ad
supotato6 <zhangjinghong@baidu.com>
2026-02-25 16:12:41 +0800

    fix comment

--- paddleformers/transformers/__init__.py
+++ paddleformers/transformers/__init__.py
@@ -229,6 +229,7 @@ import_structure = {
         "Qwen3OmniMoeTalkerModel",
     ],
     "qwen3_omni_moe.processor": ["Qwen3OmniMoeProcessor"],
+    "qwen3_omni_moe.audio_processor": ["WhisperFeatureExtractor"],
     "qwen2_moe.configuration": ["Qwen2MoeConfig"],
     "qwen2_moe.modeling": [
         "Qwen2MoeModel",
