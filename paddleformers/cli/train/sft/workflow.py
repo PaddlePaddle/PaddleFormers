7dc69db9557c70aca295c8b007e0f698adf14eb5
Ting <wtmlon@foxmail.com>
2026-02-26 11:46:56 +0800

    fix typo.

--- paddleformers/cli/train/sft/workflow.py
+++ paddleformers/cli/train/sft/workflow.py
@@ -87,12 +87,11 @@ from paddleformers.cli.utils import (
 )
 
 
-def frozen_param_expect_mtp(model, config):
+def freeze_param_except_mtp(model, config):
     def extract_layer_idx(text):
         match = re.search(r"model.layers.(-?\d+\.?\d*)", text)
         if match:
             num_str = match.group(1)
-            # 区分整数和小数返回（避免123.0这种冗余浮点数）
             if "." in num_str:
                 return float(num_str)
             else:
@@ -725,7 +724,7 @@ def run_sft(
     trainable_parameters = [
         p for p in model.parameters() if not p.stop_gradient or ("quantization_linear" in p.name and "w_1" in p.name)
     ]
-    frozen_param_expect_mtp(model, model_config)
+    freeze_param_except_mtp(model, model_config)
     trainer.set_optimizer_grouped_parameters(trainable_parameters)
 
     # Train
