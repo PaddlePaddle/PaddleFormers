4bdbe3a9e077832a939f2956ac4ae85990dc55ee
Lcysabcu <19556234170@163.com>
2026-02-26 17:05:05 +0800

    try

--- scripts/unit_test/ci_unittest.sh
+++ scripts/unit_test/ci_unittest.sh
@@ -140,10 +140,12 @@ if [[ ${FLAGS_enable_CI} == "true" ]] || [[ ${FLAGS_enable_CE} == "true" ]];then
     unset http_proxy && unset https_proxy
     set +e
     export PYTHONFAULTHANDLER=1
+    export OMP_NUM_THREADS=1
+    export MKL_NUM_THREADS=1
     DOWNLOAD_SOURCE=aistudio WAIT_UNTIL_DONE=True PADDLEFORMERS_TESTING=True \
     PYTHONPATH=$(pwd) \
     COVERAGE_SOURCE=paddleformers \
-    timeout 10m \
+    timeout 15m \
     python -m pytest -v -s -n 8 \
         --dist no \
         --maxfail=10 \
