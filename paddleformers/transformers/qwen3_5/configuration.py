1f162e096e31f0d480c641b18d7555aa30996f00
Lcysabcu <19556234170@163.com>
2026-02-28 15:35:51 +0800

    add qwen3_5 vit

---
+++ paddleformers/transformers/qwen3_5/configuration.py
@@ -0,0 +1,38 @@
+from ..configuration_utils import PretrainedConfig
+
+class Qwen3_5VisionConfig(PretrainedConfig):
+    model_type = "qwen3_5"
+    base_config_key = "vision_config"
+
+    def __init__(
+        self,
+        depth=27,
+        hidden_size=1152,
+        hidden_act="silu",
+        intermediate_size=4304,
+        num_heads=16,
+        in_channels=3,
+        patch_size=16,
+        spatial_merge_size=2,
+        temporal_patch_size=2,
+        out_hidden_size=3584,
+        num_position_embeddings=2304,
+        initializer_range=0.02,
+        **kwargs,
+    ):
+        super().__init__(**kwargs)
+
+        self.depth = depth
+        self.hidden_size = hidden_size
+        self.hidden_act = hidden_act
+        self.intermediate_size = intermediate_size
+        self.num_heads = num_heads
+        self.in_channels = in_channels
+        self.patch_size = patch_size
+        self.spatial_merge_size = spatial_merge_size
+        self.temporal_patch_size = temporal_patch_size
+        self.out_hidden_size = out_hidden_size
+        self.num_position_embeddings = num_position_embeddings
+        self.initializer_range = initializer_range
+
+__all__ = ["Qwen3_5VisionConfig"]
---
+++ paddleformers/transformers/qwen3_5/modeling.py
@@ -0,0 +1,69 @@
+import paddle
+import paddle.nn.functional as F
+from paddle import nn
+
+from ..model_outputs import BaseModelOutputWithPooling
+from .configuration import Qwen3_5VisionConfig
+from ..qwen3_vl.modeling import  Qwen3VLVisionModel, Qwen3VLVisionBlock
+from typing import Optional
+
+class Qwen3_5VisionModel(Qwen3VLVisionModel):
+    config_class = Qwen3_5VisionConfig
+    _no_split_modules = ["Qwen3VLVisionBlock"]
+
+    def __init__(self, config, *inputs, **kwargs) -> None:
+        super().__init__(config, *inputs, **kwargs)
+        if not hasattr(self, 'pos_embed'):
+            self.pos_embed = nn.Embedding(config.num_position_embeddings, config.hidden_size)
+        del self.deepstack_visual_indexes
+        del self.deepstack_merger_list
+
+    def forward(self, hidden_states: paddle.Tensor, grid_thw: paddle.Tensor, **kwargs) -> paddle.Tensor:
+        """
+        Args:
+            hidden_states (`paddle.Tensor` of shape `(seq_len, hidden_size)`):
+                The final hidden states of the model.
+            grid_thw (`paddle.Tensor` of shape `(num_images_or_videos, 3)`):
+                The temporal, height and width of feature shape of each image in LLM.
+
+        Returns:
+            `paddle.Tensor`: hidden_states.
+        """
+        hidden_states = self.patch_embed(hidden_states)
+
+        pos_embeds = self.fast_pos_embed_interpolate(grid_thw)
+        hidden_states = hidden_states + pos_embeds
+
+        rotary_pos_emb = self.rot_pos_emb(grid_thw)
+
+        seq_len, _ = hidden_states.shape
+        hidden_states = hidden_states.reshape([seq_len, -1])
+        rotary_pos_emb = rotary_pos_emb.reshape([seq_len, -1])
+        emb = paddle.concat([rotary_pos_emb, rotary_pos_emb], axis=-1)
+        position_embeddings = (paddle.cos(emb), paddle.sin(emb))
+
+        cu_seqlens = paddle.repeat_interleave(grid_thw[:, 1] * grid_thw[:, 2], grid_thw[:, 0]).cumsum(
+            axis=0,
+            # Select dtype based on the following factors:
+            #  - FA2 requires that cu_seqlens_q must have dtype int32
+            #  - paddle.onnx.export requires that cu_seqlens_q must have same dtype as grid_thw
+            dtype=grid_thw.dtype if not paddle.in_dynamic_mode() else 'int32',
+        )
+        cu_seqlens = F.pad(cu_seqlens, [1, 0], value=0)
+
+        for blk in self.blocks:
+            hidden_states = blk(
+                hidden_states,
+                cu_seqlens=cu_seqlens,
+                position_embeddings=position_embeddings,
+                **kwargs,
+            )
+
+        merged_hidden_states = self.merger(hidden_states)
+
+        return BaseModelOutputWithPooling(
+            last_hidden_state=hidden_states,
+            pooler_output=merged_hidden_states,
+        )
+
+__all__ = ["Qwen3_5VisionModel"]
