528a930e4a3b9f5352294903e0a578ce7600cef4
xiaoguoguo626807 <wangruting@baidu.com>
2026-03-10 19:47:30 +0800

    revert kimik25

--- paddleformers/transformers/auto/modeling.py
+++ paddleformers/transformers/auto/modeling.py
@@ -59,7 +59,6 @@ MAPPING_NAMES = OrderedDict(
         ("Ernie4_5_VLMoe", "ernie4_5_moe_vl"),
         ("PaddleOCRVL", "paddleocr_vl"),
         ("Llama", "llama"),
-        ("KimiK25", "kimi_k25"),
         ("KimiK2", "kimi_k2"),
         ("Qwen2", "qwen2"),
         ("Qwen2_5_VL", "qwen2_5_vl"),
