a10305d7f454aff633b2561db2fd3348e25bb36f
llbdyiu66 <llbdyiu66@users.noreply.github.com>
2026-03-10 15:54:06 +0800

    fix fleet_model config to_json_string error

--- paddleformers/trainer/integrations.py
+++ paddleformers/trainer/integrations.py
@@ -207,7 +207,7 @@ class TensorBoardCallback(TrainerCallback):
             self.tb_writer.add_text("args", args.to_json_string())
             if "model" in kwargs:
                 model = kwargs["model"]
-                if hasattr(model, "config") and model.config is not None:
+                if "gpt_provider" not in str(type(model)) and hasattr(model, "config") and model.config is not None:
                     model_config_json = model.config.to_json_string()
                     self.tb_writer.add_text("model_config", model_config_json)
 
