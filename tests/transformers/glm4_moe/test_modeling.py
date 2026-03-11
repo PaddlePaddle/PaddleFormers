35285d9f250415ec8c949b8611e7f751894ba9af
Ace-To-HYB <huangyb83@foxmail.com>
2026-03-11 13:29:05 +0800

    Fix CI

--- tests/transformers/glm4_moe/test_modeling.py
+++ tests/transformers/glm4_moe/test_modeling.py
@@ -302,7 +302,7 @@ class Glm4MoeModelTest(ModelTesterMixin, GenerationTesterMixin, unittest.TestCas
     all_model_classes = (Glm4MoeModel, Glm4MoeForCausalLM)
     all_generative_model_classes = {Glm4MoeForCausalLM: (Glm4MoeModel, "Glm4Moe")}
 
-    @gpu_device_initializer(log_prefix="Glm4MoeModelTest")
+    @gpu_device_initializer(log_prefix="Glm4MoeModelTest", gpu_id=0)
     def setUp(self):
         super().setUp()
 
@@ -390,7 +390,6 @@ class Glm4MoeModelTest(ModelTesterMixin, GenerationTesterMixin, unittest.TestCas
             # test save_pretrained
             with tempfile.TemporaryDirectory() as tmpdirname:
                 model1.save_pretrained(tmpdirname, save_checkpoint_format="flex_checkpoint")
-                model1.config.save_pretrained(tmpdirname)
                 model2 = model_class.from_pretrained(
                     tmpdirname,
                     convert_from_hf=True,
