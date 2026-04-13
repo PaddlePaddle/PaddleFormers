b2446a3cc6190a101a9dfa6d43d7b85b991c4bdf
tianlef <1095012807@qq.com>
2026-04-13 15:06:43 +0800

    [Fleet Release]Fleet CI Qwen3VL MOE add release precison (#4267)

--- tests/integration_test/qwen3vl_sft.sh
+++ tests/integration_test/qwen3vl_sft.sh
@@ -68,6 +68,10 @@ unset http_proxy https_proxy
 log_file=qwen3vl_sft_${machine}_${step}_multi_card.txt
 gt_loss_file=qwen3vl_sft_${machine}_${step}_multi_card_gt_loss.txt
 
+if [[ "$step" == "moe" && "$machine" == "h20" ]]; then
+    gt_loss_file=qwen3vl_sft_release_${machine}_${step}_multi_card_gt_loss.txt
+fi
+
 set +e
 NNODES=1 MASTER_ADDR=$master MASTER_PORT=$port coverage run $(which paddleformers-cli) train $config_yaml 2>&1 | tee ./${log_file}
 
