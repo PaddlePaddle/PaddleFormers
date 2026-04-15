026f10940c0056747bdd2d89da6117313e1f0838
tianlef <1095012807@qq.com>
2026-04-15 19:36:45 +0800

    FIX

--- tests/integration_test/check_precision_approval.sh
+++ tests/integration_test/check_precision_approval.sh
@@ -47,7 +47,7 @@ function run_tools_test() {
 }
 
 
-PRECISION_APPROVERS1="XieYunshen From00 risemeup1 tianlef lugimzzz zjjlivein"
+PRECISION_APPROVERS1="XieYunshen From00 risemeup1 tianlef"
 echo_line="You must be approved by one of ${PRECISION_APPROVERS1} for changing precision.\n"
 APPROVER_LIST1=(${PRECISION_APPROVERS1})
 check_approval 1 "${APPROVER_LIST1[@]}"
