740528378373adbfe57bdb55a371852b15feffba
Minestar6 <3197492890@qq.com>
2026-04-17 07:06:38 +0000

    second

--- paddleformers/transformers/download_utils.py
+++ paddleformers/transformers/download_utils.py
@@ -865,6 +865,23 @@ register_model_group(
     }
 )
 
+# granite
+register_model_group(
+    models={
+        "Granite-3.2-2B-Instruct": {
+            DownloadSource.HUGGINGFACE: "ibm-granite/granite-3.2-2b-instruct",
+        },
+        "Granite-3.2-8B-Instruct": {
+            DownloadSource.HUGGINGFACE: "ibm-granite/granite-3.2-8b-instruct",
+        },
+        "Granite-3.2-2B": {
+            DownloadSource.HUGGINGFACE: "ibm-granite/granite-3.2-2b",
+        },
+        "Granite-3.2-8B": {
+            DownloadSource.HUGGINGFACE: "ibm-granite/granite-3.2-8b",
+        }
+    }
+)
 
 # llama3
 register_model_group(
