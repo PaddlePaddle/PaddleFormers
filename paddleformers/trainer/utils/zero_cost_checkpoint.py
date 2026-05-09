b7be997992c446c8aaaf2f910be566eaabf3bfc3
Chen Zhiyang <1792266893@qq.com>
2026-05-09 17:29:40 +0800

    fix reshape

--- paddleformers/trainer/utils/zero_cost_checkpoint.py
+++ paddleformers/trainer/utils/zero_cost_checkpoint.py
@@ -2240,7 +2240,7 @@ class ZeroCostCheckpointWorkerFcBased(ZeroCostCheckpointWorker):
             logger.info("[ZCC worker] master weights dict filter by master_weights_filter complete.")
 
             def _extract_struct_name(key):
-                match = re.match(r"^(.*)\.(moment1_0|moment2_0)$", key)
+                match = re.match(r"^(.*)\.(moment1_0|moment2_0|w_0)$", key)
                 return match.group(1) if match else None
 
             if self.grouped_gemm_params and len(self.grouped_gemm_params) > 0:
