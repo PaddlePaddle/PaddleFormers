b20a74fdfbf145e8a3e6f67f7d76c796a934c630
zjjlivein <1834792141@qq.com>
2026-05-11 17:16:15 +0800

    update

--- scripts/unit_test/ci_unittest.sh
+++ scripts/unit_test/ci_unittest.sh
@@ -19,6 +19,7 @@ export paddle=$1
 export FLAGS_enable_CE=${2-false}
 export nlp_dir=/workspace/PaddleFormers
 export log_path=/workspace/PaddleFormers/unittest_logs
+unset http_proxy && unset https_proxy
 cd $nlp_dir
 if [ ! -d "unittest_logs" ];then
     mkdir unittest_logs
@@ -33,32 +34,12 @@ dir_name=$(dirname "${PYTEST_EXECUTE_FLAG_FILE}")
 mkdir -p "${dir_name}"
 AGILE_COMPILE_BRANCH=$4
 
-kill_process() {
-    echo -e "\033[32m===== print python / pytest / xdist processes =====\033[0m"
-
-    ps -o pid,ppid,tty,stat,etime,cmd -C python | \
-      grep -E 'pytest|exec\(eval|paddleformers|launcher\.py' || true
-
-    echo -e "\033[32m===== kill python / pytest / xdist processes =====\033[0m"
-
-    TTY=$(tty | sed 's#/dev/##')
-
-    # kill pytest + xdist on current tty
-    ps -o pid=,tty=,cmd= -C python | \
-      awk -v tty="$TTY" '$2==tty && $3 ~ /pytest|exec\(eval/ {print $1}' | \
-      xargs -r kill -9 || true
-
-    # kill paddleformers launcher (distributed training)
-    pkill -9 -f paddleformers/cli/launcher.py || true
-}
-
-
 install_requirements() {
     start_ts=$(date +%s)
     python -m pip config --user set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
     python -m pip config --user set global.trusted-host pypi.tuna.tsinghua.edu.cn
     python -m pip uninstall paddlepaddle paddlepaddle_gpu paddlefleet -y
-    python -m pip install -U --no-cache-dir transformers   > /dev/null
+    python -m pip install -U --no-cache-dir transformers -i https://pypi.org/simple  > /dev/null
     cd /home/models/my_packages && dpkg -i *.deb > /dev/null
     cd -
     # python -m pip install --no-cache-dir ${paddle} --no-dependencies --progress-bar off
@@ -74,12 +55,12 @@ install_requirements() {
         python -m pip install paddlepaddle_gpu-0.0.0-cp310-cp310-linux_x86_64.whl --extra-index-url https://www.paddlepaddle.org.cn/packages/nightly/cu129/ 
 
     else
-        pip install "$(ls -t dist/*.whl | head -1)[paddlefleet]"  --extra-index-url https://www.paddlepaddle.org.cn/packages/stable/cu129/ --extra-index-url https://www.paddlepaddle.org.cn/packages/nightly/cu129/
+        pip install "$(ls -t dist/*.whl | head -1)[paddlefleet]" -i https://pypi.org/simple --extra-index-url https://www.paddlepaddle.org.cn/packages/stable/cu129/ --extra-index-url https://www.paddlepaddle.org.cn/packages/nightly/cu129/
     fi
     echo "paddlefleet commit:"
     python -c "import paddlefleet; print(paddlefleet.version.commit)"
     python -c "import paddle;print('paddle');print(paddle.__version__);print(paddle.version.show())" >> ${log_path}/commit_info.txt
-    pip install -r tests/requirements.txt  
+    pip install -r tests/requirements.txt -i https://pypi.org/simple 
     python -c "from paddleformers import __version__; print('paddleformers version:', __version__)" >> ${log_path}/commit_info.txt
     python -c "import paddleformers; print('paddleformers commit:',paddleformers.version.commit)" >> ${log_path}/commit_info.txt
     python -m pip list >> ${log_path}/commit_info.txt 
@@ -144,11 +125,9 @@ fi
 get_diff_TO_case
 set_env
 if [[ ${FLAGS_enable_CI} == "true" ]] || [[ ${FLAGS_enable_CE} == "true" ]];then
-    kill_process
     install_requirements
     cd ${nlp_dir}
     echo ' Testing all unittest cases '
-    unset http_proxy && unset https_proxy
     export CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES}
     set +e
     export PYTHONFAULTHANDLER=1
@@ -172,7 +151,6 @@ if [[ ${FLAGS_enable_CI} == "true" ]] || [[ ${FLAGS_enable_CE} == "true" ]];then
     if [ -n "${AGILE_JOB_BUILD_ID}" ]; then
         cd ${nlp_dir}
         echo -e "\033[35m ---- Generate Allure Report  \033[0m"
-        unset http_proxy && unset https_proxy
         cp scripts/unit_test/gen_allure_report.py ./
         python gen_allure_report.py > /dev/null
         echo -e "\033[35m ---- Report: https://xly.bce.baidu.com/ipipe/ipipe-report/report/${AGILE_JOB_BUILD_ID}/report/  \033[0m"
