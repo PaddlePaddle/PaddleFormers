381b89e52d6634de4b88eb9a0bcd6d97a04007a8
liujie44 <liujie44@baidu.com>
2026-05-11 17:09:27 +0800

    update ce

--- scripts/regression/ci_model_unittest.sh
+++ scripts/regression/ci_model_unittest.sh
@@ -161,7 +161,7 @@ if [[ "$update_baseline_models" != "false" ]] && [[ "$update_baseline_models" !=
 elif [[ ${FLAGS_enable_CI} == "True" ]];then
     get_diff_TO_case
 elif [[ ${FLAGS_enable_CE} != "False" ]];then
-    models="paddleocr_vl"  ## for test  org-all
+    models="all"
 fi
 
 if [[ ${FLAGS_enable_CI} == "True" ]] || [[ ${FLAGS_enable_CE} != "False" ]];then
