488959b6a8c4cfbf7041063f4216f9ac8f150f8c
tianlef <1095012807@qq.com>
2026-05-20 14:34:40 +0800

    fix

--- scripts/ci_utils/get_fleet_commit.py
+++ scripts/ci_utils/get_fleet_commit.py
@@ -14,19 +14,19 @@
 
 import os
 
+
 # 读取setup.py文件 拿到fleet的commit id
 def get_fleet_commit_id(setup_file_path):
-    with open(setup_file_path, 'r') as f:
+    with open(setup_file_path, "r") as f:
         for line in f:
-            if 'paddlefleet==' in line:
-                commit_id = line.split('paddlefleet==')[1].strip().strip('"').strip("'")
-                commit_id = commit_id.split('+')[1].strip()  # 如果有版本号，取最后的commit id部分
+            if "paddlefleet==" in line:
+                commit_id = line.split("paddlefleet==")[1].strip().strip('"').strip("'")
+                commit_id = commit_id.split("+")[1].strip()  # 如果有版本号，取最后的commit id部分
                 commit_id = commit_id.split('"')[0].strip()  # 如果有版本号，取最后的commit id部分
                 return commit_id
     raise ValueError("FLEET_COMMIT_ID not found in setup.py")
 
 
 if __name__ == "__main__":
-    fleet_commit_id = get_fleet_commit_id('setup.py')
+    fleet_commit_id = get_fleet_commit_id("setup.py")
     print(fleet_commit_id)
-
