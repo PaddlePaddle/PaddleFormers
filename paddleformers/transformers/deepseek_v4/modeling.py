9afb2c21257f59d8f58c9481e9ae2984a219b688
wangbingguang <2603963659@qq.com>
2026-06-09 23:51:55 +0800

    删除冗余的过滤条件

--- paddleformers/transformers/deepseek_v4/modeling.py
+++ paddleformers/transformers/deepseek_v4/modeling.py
@@ -542,12 +542,11 @@ class DeepseekV4PreTrainedModel(PretrainedModel):
                 ]
 
         # === 3. Top-level mHC head contraction (output head HyperConnection) ===
-        if mtp_num_layers > 0:
-            stmts += [
-                "hc_head_base -> model.mhc_contract.hc_head_base, dtype='bfloat16'",
-                "hc_head_fn^T -> model.mhc_contract.hc_head_fn, dtype='bfloat16'",
-                "hc_head_scale -> model.mhc_contract.hc_head_scale, dtype='bfloat16'",
-            ]
+        stmts += [
+            "hc_head_base -> model.mhc_contract.hc_head_base, dtype='bfloat16'",
+            "hc_head_fn^T -> model.mhc_contract.hc_head_fn, dtype='bfloat16'",
+            "hc_head_scale -> model.mhc_contract.hc_head_scale, dtype='bfloat16'",
+        ]
 
         # === 4. MTP (Multi-Token Prediction) layers ===
         for i in range(mtp_num_layers):
