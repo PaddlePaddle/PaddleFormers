cc2e1238fd82d606d74b28edc5b712bbe32ffb95
gushiwei <gushiwei@baidu.com>
2026-07-28 12:49:20 +0000

    update fleet

--- setup.py
+++ setup.py
@@ -204,7 +204,7 @@ try:
         install_requires=REQUIRED_PACKAGES,
         entry_points={"console_scripts": get_console_scripts()},
         extras_require={
-            "paddlefleet": ["paddlefleet==0.4.0.post20260723+11bfcbad56f"],
+            "paddlefleet": ["paddlefleet==0.4.0.post20260728+712fd29a930"],
         },
         python_requires=">=3.8",
         classifiers=[
