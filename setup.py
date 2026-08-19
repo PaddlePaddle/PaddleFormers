ee8286795ed770737947a856c70175a2c8eecf9d
Wang Shigure <192935775+SigureWang@users.noreply.github.com>
2026-08-19 23:56:43 +0800

    [release/1.3][Deps] Update PaddleFleet to 33c2b9a2178

--- setup.py
+++ setup.py
@@ -204,7 +204,7 @@ try:
         install_requires=REQUIRED_PACKAGES,
         entry_points={"console_scripts": get_console_scripts()},
         extras_require={
-            "paddlefleet": ["paddlefleet==0.4.0.post20260814+66a8168b7b3"],
+            "paddlefleet": ["paddlefleet==0.4.0.post20260819+33c2b9a2178"],
         },
         python_requires=">=3.8",
         classifiers=[
