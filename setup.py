53d29fc4c48fc33fb1c8f81fa13832626122f6d0
Wang Shigure <192935775+SigureWang@users.noreply.github.com>
2026-08-21 20:00:59 +0800

    [Deps] Update PaddleFleet to 4ba2429256b

--- setup.py
+++ setup.py
@@ -204,7 +204,7 @@ try:
         install_requires=REQUIRED_PACKAGES,
         entry_points={"console_scripts": get_console_scripts()},
         extras_require={
-            "paddlefleet": ["paddlefleet==0.4.0.post20260821+cc7ea038f82"],
+            "paddlefleet": ["paddlefleet==0.4.0.post20260821+4ba2429256b"],
         },
         python_requires=">=3.8",
         classifiers=[
