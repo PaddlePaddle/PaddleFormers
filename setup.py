8147a8ee55a0a48cb5b3173b1eac9d96339c4d6b
Wang Shigure <192935775+SigureWang@users.noreply.github.com>
2026-08-21 17:48:46 +0800

    [release/1.3][Deps] Update PaddleFleet to cc7ea038f82

--- setup.py
+++ setup.py
@@ -204,7 +204,7 @@ try:
         install_requires=REQUIRED_PACKAGES,
         entry_points={"console_scripts": get_console_scripts()},
         extras_require={
-            "paddlefleet": ["paddlefleet==0.4.0.post20260814+66a8168b7b3"],
+            "paddlefleet": ["paddlefleet==0.4.0.post20260821+cc7ea038f82"],
         },
         python_requires=">=3.8",
         classifiers=[
