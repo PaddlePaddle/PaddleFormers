52406229e9d40453ffaf60c1d3be9e8c22013e6c
root <root@yq01-qianmo-com-255-134-17.yq01.baidu.com>
2026-09-08 20:14:02 +0800

    ⬆️ chore(deps): update PaddleFleet on release/1.3

--- setup.py
+++ setup.py
@@ -204,7 +204,7 @@ try:
         install_requires=REQUIRED_PACKAGES,
         entry_points={"console_scripts": get_console_scripts()},
         extras_require={
-            "paddlefleet": ["paddlefleet==0.4.0.post20260905+6d69f843df9"],
+            "paddlefleet": ["paddlefleet==0.4.0.post20260908+8a8b7e70495"],
         },
         python_requires=">=3.8",
         classifiers=[
